b89bd828b03c6ae804f2f2f678b2a6a570932c76
Vladyslav Goncharuk <svlad1990@gmail.com>
2021-06-28 21:41:26 +0300

    - Soldier animation created
    - Player script adjusted

--- assets/scripts/followingNode.ts
+++ assets/scripts/followingNode.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec3, UITransform } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('FollowingObject')
@@ -8,11 +8,27 @@ export class FollowingObject extends Component
     @property
     followedObject:Node = new Node();
 
+    @property
+    adjustmentX:number = 0;
+
+    @property
+    adjustmentY:number = 0;
+
     update (deltaTime:number)
     {
-        var targetPosition = this.followedObject.position;
-        var currentPosition = this.node.position.clone();
-        currentPosition.lerp( targetPosition, 0.1 );
-        this.node.setPosition( currentPosition.x, currentPosition.y );
+        if(null != this.followedObject)
+        {
+            var followedObjectUITransform = this.followedObject.getComponent(UITransform);
+
+            if(followedObjectUITransform != null)
+            {
+                var targetPosition = this.followedObject.position.clone();
+                targetPosition.x += followedObjectUITransform.width * this.followedObject.scale.x * this.adjustmentX;
+                targetPosition.y += followedObjectUITransform.height * this.followedObject.scale.y * this.adjustmentY;
+                var currentPosition = this.node.position.clone();
+                currentPosition.lerp( targetPosition, 0.1 );
+                this.node.setPosition( currentPosition.x, currentPosition.y );
+            }
+        }
     }
 }
